439a0486d318bb97b438bebd63c8c8b36a13407c
imnashu07 <imnashu07@gmail.com>
2021-12-15 15:11:33 +0530

    Final-code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Covid from './Components/Covid';
+import livebutton from './livebutton.svg'
+
+function App() {
+  
+  return (
+    <div className='App'>
+      <h3>Updating-Live</h3>
+      <img src={livebutton} width={120} alt='live' />
+      <h1>Covid 19 Karnataka Tracker</h1>
+      <Covid/>
+    </div>
+  );
+}
+
+export default App;
